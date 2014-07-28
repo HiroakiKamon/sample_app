2653d02b652bde9bef875a3af17052f4c251031e
Kamon <Hiroaki@Hiroaki-MacBook-Pro.local>
2014-07-28 12:43:24 +0900

    support heroku upload

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,10 @@ SampleApp::Application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+
+#config.active_record.dump_schema_after_migration = false
+
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
