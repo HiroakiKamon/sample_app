45e140da36bf66ace4b0774883081975c09f228d
Kamon <Hiroaki@Hiroaki-MacBook-Pro.local>
2014-08-14 14:29:47 +0900

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,14 +19,17 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
 
+
   def show
-    @user = User.find(params[:id])
+      @user = User.find(params[:id])
+      @microposts = @user.microposts.paginate(page: params[:page])
   end
+
 
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
+      flash[:success] = "updated"
       redirect_to @user
     else
       render 'edit'
@@ -57,12 +60,8 @@ class UsersController < ApplicationController
 
   # Before actions
 
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in."
-    end
-  end
+
+
 
   def correct_user
     @user = User.find(params[:id])
